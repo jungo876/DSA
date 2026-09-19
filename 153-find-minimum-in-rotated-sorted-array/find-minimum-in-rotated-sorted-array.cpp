fc9c0ab36ac22d73e4e1148ebf3811ae73450cd3
Arghya Kanji <106514387+jungo876@users.noreply.github.com>
2026-09-19 18:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (11.43%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,6 +6,10 @@ public:
         int ans = INT_MAX;
         while(low<=high){
             int d = (low+high)/2;
+            if(nums[low]<=nums[high]){
+                ans = min(ans,nums[low]);
+                break;
+            }
             if(nums[low]<=nums[d]){
                 if(nums[low]<ans){
                     ans = nums[low];
